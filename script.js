1b0df10ae9c1b617c7fee47e7dac25179f9435c7
Ayush Sahu <115640976+ayushS22@users.noreply.github.com>
2025-08-01 11:24:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+  // Leap year rules:
+  // - Every year divisible by 4 is a leap year
+  // - EXCEPT years divisible by 100, unless also divisible by 400
+  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    return 366; // Leap year
+  } else {
+    return 365; // Regular year
+  }
+}
